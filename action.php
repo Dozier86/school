47431ed8ea1ff35b64738ea150b81b8e807a2667
Dozier86 <70452198+Dozier86@users.noreply.github.com>
2020-08-29 15:56:02 -0500

    Add files via upload

---
+++ action.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Thank you <?php echo htmlspecialchars($_POST['fname']; ?> for your feedback.</p><br>
+
+    <div style="text-align:center">
+        <a href="file:///H:/OneDrive/DeVry/Classes/9July2020/CIS363B/Project/index.html">Home</a>
+    </div>
+
+</body>
+</html>
